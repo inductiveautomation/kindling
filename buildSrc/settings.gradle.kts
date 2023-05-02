98a14d36f924f05e12808e731dccc6505e8f2292
Paul Griffith <39345262+paul-griffith@users.noreply.github.com>
2023-05-02 16:00:10 -0700

    Publish version 1.0.0

--- buildSrc/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "kindling-build"
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
-}
