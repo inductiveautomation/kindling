798ee061eeb666091269828540c6f7736da4d90a
Cosmo Stevens <justcosmo@ymail.com>
2024-03-15 18:07:32 -0700

    Added jump to next/previous log feature (#128)
    Co-authored-by: Paul Griffith <pgriffith@ia.io>

--- src/main/kotlin/io/github/inductiveautomation/kindling/thread/PoolPanel.kt
+++ src/main/kotlin/io/github/inductiveautomation/kindling/thread/PoolPanel.kt
@@ -1,5 +1,6 @@
 package io.github.inductiveautomation.kindling.thread
 
+import com.formdev.flatlaf.extras.FlatSVGIcon
 import io.github.inductiveautomation.kindling.thread.model.Thread
 import io.github.inductiveautomation.kindling.utils.FilterListPanel
 
@@ -7,5 +8,7 @@ class PoolPanel : FilterListPanel<Thread?>(
     tabName = "Pool",
     toStringFn = { it?.toString() ?: "(No Pool)" },
 ) {
+    override val icon = FlatSVGIcon("icons/bx-chip.svg")
+
     override fun filter(item: Thread?) = item?.pool in filterList.checkBoxListSelectedValues
 }
