575a3bc6ec2612d3d56c32b77f26a7c2496e7cc6
Paul Griffith <pgriffith@ia.io>
2024-03-25 09:06:27 -0700

    Change QuarantineViewer to use a JList, add XML viewer to readme

--- src/main/kotlin/io/github/inductiveautomation/kindling/xml/quarantine/QuarantineViewer.kt
+++ src/main/kotlin/io/github/inductiveautomation/kindling/xml/quarantine/QuarantineViewer.kt
@@ -1,25 +1,65 @@
 package io.github.inductiveautomation.kindling.xml.quarantine
 
+import deser.SerializationDumper
 import io.github.inductiveautomation.kindling.core.Detail
 import io.github.inductiveautomation.kindling.core.DetailsPane
+import io.github.inductiveautomation.kindling.utils.FlatScrollPane
+import io.github.inductiveautomation.kindling.utils.HorizontalSplitPane
 import io.github.inductiveautomation.kindling.utils.XML_FACTORY
 import io.github.inductiveautomation.kindling.utils.deserializeStoreAndForward
 import io.github.inductiveautomation.kindling.utils.parse
 import io.github.inductiveautomation.kindling.utils.toDetail
 import io.github.inductiveautomation.kindling.xml.XmlTool
 import net.miginfocom.swing.MigLayout
-import java.awt.EventQueue
-import java.awt.Rectangle
+import javax.swing.JList
 import javax.swing.JPanel
+import javax.swing.ListSelectionModel.MULTIPLE_INTERVAL_SELECTION
 import com.inductiveautomation.ignition.common.Base64 as IaBase64
 
-class QuarantineViewer(data: List<Detail>) : JPanel(MigLayout("ins 6, fill, hidemode 3")) {
+internal class QuarantineViewer(data: List<QuarantineRow>) : JPanel(MigLayout("ins 6, fill, hidemode 3")) {
+    private val list = JList(Array(data.size) { i -> i + 1 }).apply {
+        selectionMode = MULTIPLE_INTERVAL_SELECTION
+    }
+
+    private val detailsPane = DetailsPane()
+
     init {
-        val detailsPane = DetailsPane(data)
-        EventQueue.invokeLater {
-            detailsPane.scrollRectToVisible(Rectangle(0, 0))
+        list.addListSelectionListener {
+            detailsPane.events = list.selectedIndices.map { i ->
+                data[i].detail
+            }
         }
-        add(detailsPane, "push, grow")
+
+        add(
+            HorizontalSplitPane(
+                FlatScrollPane(list),
+                detailsPane,
+                0.1,
+            ),
+            "push, grow",
+        )
+    }
+
+    internal data class QuarantineRow(
+        val b64data: String,
+    ) {
+        private val binaryData: ByteArray by lazy {
+            IaBase64.decodeAndGunzip(b64data)
+        }
+
+        val detail by lazy {
+            try {
+                binaryData.deserializeStoreAndForward().toDetail()
+            } catch (e: Exception) {
+                XmlTool.logger.error("Unable to deserialize quarantine data", e)
+                val serializedData = SerializationDumper(binaryData).parseStream().lines()
+                Detail(
+                    title = "Error",
+                    message = "Failed to deserialize: ${e.message}",
+                    body = serializedData,
+                )
+            }
+        }
     }
 
     companion object {
@@ -28,20 +68,8 @@ class QuarantineViewer(data: List<Detail>) : JPanel(MigLayout("ins 6, fill, hide
             val cacheEntries = document.getElementsByTagName("base64")
 
             val data = (0..<cacheEntries.length)
-                .mapNotNull(cacheEntries::item)
-                .map { it.textContent }
-                .map { IaBase64.decodeAndGunzip(it) }
-                .map { bytes ->
-                    try {
-                        bytes.deserializeStoreAndForward().toDetail()
-                    } catch (e: Exception) {
-                        XmlTool.logger.error("Unable to deserialize quarantine data", e)
-                        Detail(
-                            title = "Error",
-                            message = "Unable to deserialize quarantine data",
-                            body = listOfNotNull(e.message),
-                        )
-                    }
+                .map { i ->
+                    QuarantineRow(cacheEntries.item(i).textContent)
                 }
 
             return if (data.isNotEmpty()) {
