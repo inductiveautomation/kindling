b519067b72f30c9aa6c9c1f2b003793eca7e6098
Spencer Lommel <118965093+SpencerLommel@users.noreply.github.com>
2025-07-22 11:00:01 -0500

    Prevent message log unmarking (#287)

--- src/main/kotlin/io/github/inductiveautomation/kindling/log/LogPanel.kt
+++ src/main/kotlin/io/github/inductiveautomation/kindling/log/LogPanel.kt
@@ -234,7 +234,11 @@ sealed class LogPanel<T : LogEvent>(
                             add(
                                 Action("Mark all with same message") {
                                     model.markRows { row ->
-                                        row.message == event.message
+                                        if (row.marked) {
+                                            null // Leave existing marked messages marked
+                                        } else {
+                                            row.message == event.message
+                                        }
                                     }
                                 },
                             )
